0392c024be831e74f0b0a243c2703bf08d01d060
Ayush-dvplr <ayush.dvlpr@gmail.com>
2024-06-23 09:52:04 +0530

    few changes

--- frontend/src/contractFunctions.js
+++ frontend/src/contractFunctions.js
@@ -4,7 +4,7 @@ import DailyPlanner from "./DailyPlanner.json";
 // NOTE:
 // if http://localhost:8545 not working then try this http://127.0.0.1:8545/
 
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; 
 
 async function connectWeb3() {
   const provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545/");
